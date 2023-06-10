d5db4e4b64b4c65e5877c218a4399d3dd5f8cc9c
吴习章 <wuxz14@vanke.com>
2023-06-10 12:04:24 +0800

    feat: blog update

--- src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
+++ src/03.5-type-helpers-pattern/20.3-multiple.problem.ts
@@ -1,8 +1,8 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type CreateDataShape = {
-  data: unknown;
-  error: unknown;
+type CreateDataShape<T1, T2> = {
+  data: T1;
+  error: T2;
 };
 
 type tests = [
@@ -32,5 +32,5 @@ type tests = [
         error: SyntaxError;
       }
     >
-  >,
+  >
 ];
