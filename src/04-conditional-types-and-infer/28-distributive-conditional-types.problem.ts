958cf36b641cebf6249322e0f64140fe7287c969
吴习章 <wuxz14@vanke.com>
2023-06-11 12:00:29 +0800

    feat: blog update

--- src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -1,7 +1,12 @@
+import { Extends } from "ts-toolbelt/out/Any/Extends";
 import { Equal, Expect } from "../helpers/type-utils";
 
 type Fruit = "apple" | "banana" | "orange";
 
-type AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
+type AppleOrBanana = Fruit extends infer T
+  ? T extends "apple" | "banana"
+    ? T
+    : never
+  : never;
 
 type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
